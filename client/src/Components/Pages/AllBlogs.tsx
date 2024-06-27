0b03144c8becb102b1dd073f57c7c0ec1b74b255
danishsalmani <salmanidanish488@gmail.com>
2024-06-28 00:56:45 +0530

    all blog page created

--- client/src/Components/Pages/AllBlogs.tsx
+++ client/src/Components/Pages/AllBlogs.tsx
@@ -2,23 +2,37 @@ import { useEffect, useState } from "react"
 import { TopBar } from "../TopBar"
 import axios from "../axios/axiosConfig";
 
+interface Blog {
+    id: string
+    title: string
+    content: string
+}
 export function AllBlogs() {
-    const [blogs,setBlogs] = useState("");
+    const [blogs, setBlogs] = useState<Blog[]>([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         axios.get("/blog/bulk")
-        .then(res=>{
-            setBlogs(res.data.blogs);
-        })
-    })
-     return (
+            .then(res => {
+                setBlogs(res.data.blogs);
+                console.log(res.data.blogs);
+            })
+    },[])
+    return (
         <div>
-            <TopBar/>
+            <TopBar />
             <div className=" flex justify-center border">
-            <div className="w-9/12">
-
-            </div>
+                <div className="w-8/12">
+                    <div className="sections">
+                            {blogs.map((blog) => (
+                                <div className="border-b ">
+                                    <div className="w-full h-12 text-3xl font-bold">{blog.title}</div>
+                                    <div className="w-full h-20 text-xl">{blog.content}</div>
+                                    </div>
+                            ))}
+                        
+                    </div>
+                </div>
             </div>
         </div>
-     )
+    )
 }
--- client/src/Components/SigninCard.tsx
+++ client/src/Components/SigninCard.tsx
@@ -1,4 +1,30 @@
+import axios from "../Components/axios/axiosConfig";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+
 export function SigninCard() {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  function handle(){
+    if(email=="" || password ==""){
+      toast.error("Fields cannot be empty")
+      return
+    }
+      axios.post("/user/signin",{
+        email,
+        password
+      },{headers:{
+        Authorization:`Bearer + ${localStorage.getItem("token")}`
+      }})
+      .then((res)=>{
+        toast.success("successful")
+      })
+      .then((res)=>{
+        toast.error(res)
+      })
+  }
     return (
       <div className=" flex flex-col justify-center items-center h-screen">
         <div className="space-y-4 bg-white rounded-lg p-4 ">
@@ -7,10 +33,10 @@ export function SigninCard() {
             <div>Don't have an account? <span className="underline">Register</span></div>
           </div>
           <div>Email</div>
-          <input type="text" placeholder="johndoe@email.com" className="w-full" />
+          <input type="text" name="email" placeholder="johndoe@email.com" className="w-full" onChange={e=>{setEmail(e.target.value)}} />
           <div>Password</div>
-          <input type="text" placeholder="password" className="w-full" />
-          <div className="cursor-pointer text-center bg-black text-white rounded p-2">Sign In</div>
+          <input type="text" name="password" placeholder="password" className="w-full" onChange={e=>{setPassword(e.target.value)}} />
+          <div className="cursor-pointer text-center bg-black text-white rounded p-2 " onClick={handle}>Sign In</div>
         </div>
       </div>
     );
