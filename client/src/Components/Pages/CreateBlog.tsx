96e3788b201d9c91fa7554ceac4edded58b41cf2
danishsalmani <salmanidanish488@gmail.com>
2024-06-29 16:21:24 +0530

    create Blog Page logic written

--- client/src/Components/Pages/CreateBlog.tsx
+++ client/src/Components/Pages/CreateBlog.tsx
@@ -1,14 +1,49 @@
+import { useState } from "react"
 import { TopBar } from "../TopBar"
-export function CreateBlog(){
-    return(
+import { toast } from "react-toastify";
+import axios from "../axios/axiosConfig";
+
+export function CreateBlog() {
+
+const [title,setTitle] = useState<string>("");
+const [content,setContent] = useState<string>("");
+
+async function handle(){
+
+    if(title=="" || content==""){
+        toast.error("Fields canno be empty");
+        return
+    }
+    try {
+        await axios.post("/blog/",{
+            title,
+            content
+        })
+        .then((response)=>{
+            if(response.data){
+                toast.success("Blog Created Successfully")
+            }else{
+                toast.error(response.data.error);
+            }
+        })
+    } catch (error:any) {
+        toast.error(error)
+    }
+}
+
+    return (
         <div>
-            <TopBar/>
-            <div className="flex justify-center">
-        <div className=" w-10/12 p-4">
-                <input type="text" placeholder=" |Title" className="text-3xl w-full" />
-                <textarea placeholder="Tell your story..." className="p-2 w-full h-screen"/>
-        </div>
-        </div></div>
+            <TopBar />
+            <div className="flex justify-center ">
+                <div className=" w-8/12 p-4 rounded-lg">
+                    <input type="text" placeholder=" |Title" className="text-3xl mb-2 w-full  "onChange={(e)=>{setTitle(e.target.value)}} />
+                    <textarea placeholder="Tell your story..." className="p-2 w-full h-screen "onChange={(e)=>{setContent(e.target.value)}}/>
+                    <div className="flex justify-center">
+                    <div className="bg-red-500 px-2 cursor-pointer border rounded-lg "onClick={handle}>Create</div>
+                </div>
+                </div>
+            </div>
+            </div>
 
     )
 }
--- client/src/Components/TopBar.tsx
+++ client/src/Components/TopBar.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
+
 export function TopBar(){
+    const navigate = useNavigate()
     return (
         <div className="py-2 px-8 flex justify-between border-b">
             <div className="flex items-center text-xl">
@@ -6,8 +10,16 @@ export function TopBar(){
                 <div>Medium </div>
             </div>
             <div className="flex items-center">
-                <div className="mx-2 px-2 cursor-pointer border rounded-lg ">Publish</div>
-                <div className="mx-2 px-2 cursor-pointer border rounded-lg ">Logout</div>
+                <div className="mx-2 px-2 cursor-pointer border rounded-lg "
+                onClick={()=>{
+                    navigate("/blog")
+                }}>Publish</div>
+                <div className="mx-2 px-2 cursor-pointer border rounded-lg "
+                onClick={()=>{
+                    localStorage.removeItem("token")
+                    navigate("/")
+                    toast.success("Logout")
+                }}>Logout</div>
             </div>
         </div>
     )
