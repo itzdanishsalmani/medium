1e985006d3885ec70a302cb93bb39a9271832e86
danishsalmani <salmanidanish488@gmail.com>
2024-07-01 09:28:05 +0530

    main.tsx filex

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ToastContainer } from 'react-toastify';
+import { Bounce, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -19,6 +19,6 @@ pauseOnFocusLoss
 draggable
 pauseOnHover
 theme="light"
-transition:Bounce/>  
+transition={Bounce}/>  
 </React.StrictMode>,
 )
